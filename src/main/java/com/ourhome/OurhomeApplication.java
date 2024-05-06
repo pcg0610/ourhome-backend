068929a547b3bef1b119de6d063ba489b52658b9
Changyu Park <changyu9906@gmail.com>
2024-05-07 00:05:21 +0900

    init SpringBoot Application

---
+++ src/main/java/com/ourhome/OurhomeApplication.java
@@ -0,0 +1,13 @@
+package com.ourhome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OurhomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OurhomeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ourhome/OurhomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ourhome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OurhomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
