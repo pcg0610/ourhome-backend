8754dc04202543936bf02f2530648fbb01f22390
PJH <parkjh0858@gmail.com>
2024-05-16 14:09:00 +0900

    feat: user service 구현

---
+++ src/main/java/com/ourhome/auth/service/UserServiceImpl.java
@@ -0,0 +1,24 @@
+package com.ourhome.auth.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ourhome.auth.dao.UserDao;
+import com.ourhome.auth.entity.User;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+	@Autowired
+	private UserDao userDao;
+	
+	@Override
+	public int insertUser(User user) {
+		return userDao.insertUser(user);
+	}
+
+	@Override
+	public User selectUser(String id) {
+		return userDao.selectUser(id);
+	}
+}
