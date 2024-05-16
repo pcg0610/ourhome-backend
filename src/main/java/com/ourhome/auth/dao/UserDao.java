e37c897d131e3811ea1a544f5f425acc3c4b26e0
PJH <parkjh0858@gmail.com>
2024-05-16 14:08:12 +0900

    feat: userDao interface 구현

---
+++ src/main/java/com/ourhome/auth/dao/UserDao.java
@@ -0,0 +1,18 @@
+package com.ourhome.auth.dao;
+
+import com.ourhome.auth.entity.User;
+
+public interface UserDao {
+	/**
+	 * front에서 입력한 사용자 정보를 바탕으로 DB에 사용자 정보를 저장
+	 * @param user : 회원가입을 희망하는 사용자 객체
+	 * @return : 회원가입 성공 시 1, 실패 시 0 반환
+	 */
+	public int insertUser(User user);
+	
+	// 로그인을 위한 회원 존재 여부 확인
+	// param: 사용자의 id
+	// return: 성공 시 1, 실패 시 0
+	// 사용자의 id가 db에 존재하는지 확인
+	public User selectUser(String id);
+}
