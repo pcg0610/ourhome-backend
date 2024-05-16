49d670f3d8980e34109e4527372b2e720275b776
PJH <parkjh0858@gmail.com>
2024-05-16 14:08:42 +0900

    feat: userService interface 추가

---
+++ src/main/java/com/ourhome/auth/service/UserService.java
@@ -0,0 +1,11 @@
+package com.ourhome.auth.service;
+
+import com.ourhome.auth.entity.User;
+
+public interface UserService {
+	// 회원가입
+	public int insertUser(User user);
+	
+	// 로그인을 위한 사용자 조회
+	public User selectUser(String id);
+}
