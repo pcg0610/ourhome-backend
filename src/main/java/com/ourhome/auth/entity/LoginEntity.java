f3108577480f3e03d79801795bdbb21099ba4dfc
PJH <parkjh0858@gmail.com>
2024-05-18 00:38:27 +0900

    feat: Login을 위한 class 생성

---
+++ src/main/java/com/ourhome/auth/entity/LoginEntity.java
@@ -0,0 +1,34 @@
+package com.ourhome.auth.entity;
+
+public class LoginEntity {
+	private String id;
+	private String password;
+	
+	public LoginEntity() {
+		
+	}
+
+	public LoginEntity(String id, String password) {
+		this.id = id;
+		this.password = password;
+	}
+	
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+
+}
