eebfce3f1d0e79ebf5549442a879fc0dd73400dc
PJH <parkjh0858@gmail.com>
2024-05-18 00:34:02 +0900

    feat: ID중복 체크 기능 추가

--- src/main/java/com/ourhome/auth/controller/UserController.java
+++ src/main/java/com/ourhome/auth/controller/UserController.java
@@ -6,10 +6,13 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ourhome.auth.entity.LoginEntity;
 import com.ourhome.auth.entity.User;
 import com.ourhome.auth.service.UserService;
 
@@ -29,9 +32,10 @@ public class UserController {
 	 * @param id : 사용자 id
 	 * @return : user가 존재하는 경우 OK(200) 코드를, 존재하지 않는다면 NO_CONTENT(204)코드 반환
 	 */
-	@GetMapping("/login")
-	public ResponseEntity<?> login(String id, String password) {
-		User user = userService.selectUser(id, password);
+	@PostMapping("/login")
+	public ResponseEntity<?> login(@RequestBody LoginEntity userEntity) {
+		System.out.println(userEntity.getId() + " " + userEntity.getPassword());
+		User user = userService.selectUser(userEntity.getId(), userEntity.getPassword());
 		return new ResponseEntity<>(user, user != null ? HttpStatus.OK : HttpStatus.NO_CONTENT);
 	}
 	
@@ -41,8 +45,21 @@ public class UserController {
 	 * @return : 회원가입에 성공하는 경우 CREATED(201)코드를, 회원가입에 실패하는 경우 BAD_REQUEST(400)반환
 	 */
 	@PostMapping("/signup")
-	public ResponseEntity<?> signUp(User user) {
+	public ResponseEntity<?> signUp(@RequestBody User user) {
+		System.out.println(user.toString());
 		int check = userService.insertUser(user);
 		return new ResponseEntity<>(check, check == 1 ? HttpStatus.CREATED : HttpStatus.BAD_REQUEST);
+	}
+	
+	@GetMapping("/checkID/{userId}")
+	public ResponseEntity<?> checkID(@PathVariable String userId) {
+		System.out.println(userId);
+		int count = userService.checkUserID(userId);
+		System.out.println("count : " + count);
+		if (count == 0) {
+			return ResponseEntity.ok().build();
+		}
+		
+		return ResponseEntity.badRequest().build();
 	}
 }
