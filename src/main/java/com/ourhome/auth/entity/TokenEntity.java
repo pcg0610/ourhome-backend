cf2e701d68d2fe9eea6c2efb7e1fd16a5595d0a5
PJH <parkjh0858@gmail.com>
2024-05-20 17:31:55 +0900

    feat: Token관리 객체 갠건

--- src/main/java/com/ourhome/auth/entity/TokenEntity.java
+++ src/main/java/com/ourhome/auth/entity/TokenEntity.java
@@ -1,5 +1,7 @@
 package com.ourhome.auth.entity;
 
+import java.util.Date;
+
 /**
  * 각 사용자의 Token 정보를 관리하기 위한 Entity
  */
@@ -10,8 +12,13 @@ package com.ourhome.auth.entity;
 public class TokenEntity {
 	private String userId;
 	private String token;
+	private String hashedToken;
 	private long issuedAt;
 	private long expiration;
+	
+	public TokenEntity() {
+		
+	}
 	
 	public TokenEntity(String userId, String token, long issuedAt, long expiration) {
 		this.userId = userId;
@@ -46,6 +53,23 @@ public class TokenEntity {
 	}
 	public void setExpiration(long expiration) {
 		this.expiration = expiration;
+	}
+	
+	// DB 저장용
+	public Date getExpirationDate() {
+		return new Date(expiration);
+	}
+	
+	public Date getIssuedAtDate() {
+		return new Date(issuedAt);
+	}
+	
+	public String getHashedToken() {
+		return hashedToken;
+	}
+
+	public void setHashedToken(String hashedToken) {
+		this.hashedToken = hashedToken;
 	}
 	
 	@Override
