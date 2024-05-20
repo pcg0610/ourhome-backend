9d4555564575a9176b68f66c82bf1dd57cb9f1a5
PJH <parkjh0858@gmail.com>
2024-05-21 03:44:57 +0900

    feat: Mypage 정보 출력을 위한 객체 생성

---
+++ src/main/java/com/ourhome/auth/entity/MyPageEntity.java
@@ -0,0 +1,81 @@
+package com.ourhome.auth.entity;
+
+import java.util.Date;
+import java.util.List;
+
+public class MyPageEntity {
+	private String name;
+	private String userId;
+	private String gender;
+	private Date birth;
+	
+	private String img;
+	private List<String> items;
+	
+	public MyPageEntity() {
+		
+	}
+
+	public MyPageEntity(String name, String userId, String gender, Date birth, String img, List<String> items) {
+		this.name = name;
+		this.userId = userId;
+		this.gender = gender;
+		this.birth = birth;
+		this.img = img;
+		this.items = items;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public Date getBirth() {
+		return birth;
+	}
+
+	public void setBirth(Date birth) {
+		this.birth = birth;
+	}
+
+	public String getImg() {
+		return img;
+	}
+
+	public void setImg(String img) {
+		this.img = img;
+	}
+
+	public List<String> getItems() {
+		return items;
+	}
+
+	public void setItems(List<String> items) {
+		this.items = items;
+	}
+
+	@Override
+	public String toString() {
+		return "MyPageEntity [name=" + name + ", userId=" + userId + ", gender=" + gender + ", birth=" + birth
+				+ ", img=" + img + ", items=" + items + "]";
+	}
+}
