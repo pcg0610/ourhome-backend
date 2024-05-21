951c524c98e40ddd8bacd42507f770637458f01f
PJH <parkjh0858@gmail.com>
2024-05-21 17:03:11 +0900

    feat: 전화번호 속성 추가

--- src/main/java/com/ourhome/auth/entity/User.java
+++ src/main/java/com/ourhome/auth/entity/User.java
@@ -1,6 +1,7 @@
 package com.ourhome.auth.entity;
 
 import java.util.Date;
+import java.util.List;
 
 /*
  * 사용자 정보를 담고 있는 Entity
@@ -9,32 +10,38 @@ public class User {
 	private long id;
 	private String userId;
 	private String password;
+	private String phoneNumber;
 	private String name;
 	private Date birth;
 	private char gender;
 	private String image;
 	
+	private List<String> personality;
+	
 	public User() {
 		
 	}
-
-	public User(String userId, String password, String name, Date birth, char gender, String image) {
+	
+	public User(long id, String userId, String password, String phoneNumber, String name, Date birth, char gender,
+			String image, List<String> personality) {
+		this.id = id;
 		this.userId = userId;
 		this.password = password;
+		this.phoneNumber = phoneNumber;
 		this.name = name;
 		this.birth = birth;
 		this.gender = gender;
 		this.image = image;
+		this.personality = personality;
 	}
 
 	public long getId() {
 		return id;
 	}
-	
+
 	public String getUserId() {
 		return userId;
 	}
-
 	public void setUserId(String userId) {
 		this.userId = userId;
 	}
@@ -42,15 +49,20 @@ public class User {
 	public String getPassword() {
 		return password;
 	}
-
 	public void setPassword(String password) {
 		this.password = password;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
 	}
 
 	public String getName() {
 		return name;
 	}
-
 	public void setName(String name) {
 		this.name = name;
 	}
@@ -58,7 +70,6 @@ public class User {
 	public Date getBirth() {
 		return birth;
 	}
-
 	public void setBirth(Date birth) {
 		this.birth = birth;
 	}
@@ -66,7 +77,6 @@ public class User {
 	public char getGender() {
 		return gender;
 	}
-
 	public void setGender(char gender) {
 		this.gender = gender;
 	}
@@ -74,14 +84,21 @@ public class User {
 	public String getImage() {
 		return image;
 	}
-
 	public void setImage(String image) {
 		this.image = image;
 	}
 
+	public List<String> getPersonality() {
+		return personality;
+	}
+	public void setPersonality(List<String> personality) {
+		this.personality = personality;
+	}
+
 	@Override
 	public String toString() {
-		return "User [userId=" + userId + ", password=" + password + ", name=" + name + ", birth=" + birth + ", gender="
-				+ gender + ", image=" + image + "]";
+		return "User [id=" + id + ", userId=" + userId + ", password=" + password + ", phoneNumber=" + phoneNumber
+				+ ", name=" + name + ", birth=" + birth + ", gender=" + gender + ", image=" + image + ", personality="
+				+ personality + "]";
 	}
 }
