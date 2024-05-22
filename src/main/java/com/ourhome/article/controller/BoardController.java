a0d765e11ced5b88a9cf8ae5768e3a1f7840f21c
PJH <parkjh0858@gmail.com>
2024-05-22 16:38:12 +0900

    feat: 게시글 조회 기능 추가

--- src/main/java/com/ourhome/article/controller/BoardController.java
+++ src/main/java/com/ourhome/article/controller/BoardController.java
@@ -1,6 +1,8 @@
 package com.ourhome.article.controller;
 
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ourhome.article.entity.ArticleEntity;
@@ -26,6 +29,7 @@ public class BoardController {
 	@Autowired
 	private ArticleService articleService;
 	
+	// 각 집에 대하여 게시글을 추가한다.
 	@PostMapping("/write")
 	@Operation(
 			description ="게시글 추가",
@@ -47,10 +51,17 @@ public class BoardController {
 		return null;
 	}
 	
+	// 각 집에 대하여 사람들이 남긴 게시글을 조회
+	@GetMapping("/home/list")
+	public ResponseEntity<?> getArticleListByHome(@RequestParam long homeId) {
+		List<ArticleEntity> articleList = articleService.getArticleListByHome(homeId);
+		return new ResponseEntity<> (articleList, articleList != null ? HttpStatus.OK : HttpStatus.BAD_REQUEST);
+	}
 	
-	@GetMapping("/list")
-	public ResponseEntity<?> getArticleList() {
-		return null;
+	@GetMapping("/id/list")
+	public ResponseEntity<?> getArticleListById(@RequestParam long userId) {
+		List<ArticleEntity> articleList = articleService.getArticleListById(userId);
+		return new ResponseEntity<> (articleList, articleList != null ? HttpStatus.OK : HttpStatus.BAD_REQUEST);
 	}
 	
 	@DeleteMapping("/remove")
