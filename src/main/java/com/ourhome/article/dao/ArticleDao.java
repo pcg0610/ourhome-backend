408f9db900166390ddf37bdd616deb360a4d5aa1
PJH <parkjh0858@gmail.com>
2024-05-22 16:38:38 +0900

    feat: 게시글 불러오기 dao 추가

--- src/main/java/com/ourhome/article/dao/ArticleDao.java
+++ src/main/java/com/ourhome/article/dao/ArticleDao.java
@@ -1,5 +1,7 @@
 package com.ourhome.article.dao;
 
+import java.util.List;
+
 import org.springframework.stereotype.Repository;
 
 import com.ourhome.article.entity.ArticleEntity;
@@ -7,5 +9,6 @@ import com.ourhome.article.entity.ArticleEntity;
 @Repository
 public interface ArticleDao {
 	int writeArticle(ArticleEntity article);
-
+	List<ArticleEntity> getArticleListByHome(long homeId);
+	List<ArticleEntity> getArticleListById(long userId);
 }
